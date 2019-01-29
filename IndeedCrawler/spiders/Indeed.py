cf84f7d8c68c09f1067450347eaf6d3a55742bc5
coviller <romain.coville@edu.esiee.fr>
2019-01-29 21:00:24 +0100

    Scaping de toutes les pages

--- IndeedCrawler/spiders/Indeed.py
+++ IndeedCrawler/spiders/Indeed.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy import Request
 
 class IndeedSpider(scrapy.Spider):
     name = 'Indeed'
     allowed_domains = ['indeed.fr']
-    start_urls = ['https://www.indeed.fr/jobs?q=data+scientist','https://www.indeed.fr/jobs?q=data+analyst','https://www.indeed.fr/jobs?q=data+engineer']
+    start_urls = ['https://www.indeed.fr/jobs?q=data+scientist&start=','https://www.indeed.fr/jobs?q=data+analyst&start=','https://www.indeed.fr/jobs?q=data+engineer&start=']
 
     def parse(self, response):
+        i = 0
+        all_links = []
+        for i in range(10):
+            j = i*10
+            all_links.append(str(response).split(" ")[1][:-1] + str(j))
+
+        for link in all_links :
+            yield Request(link, callback=self.parse_category)
+
+    def parse_category(self, response):
 
         # for item in response.css("#SALARY_rbo").css('a'):
         #     print(re.findall("\d+", item.css("::attr(title)").extract_first()))
@@ -23,6 +33,13 @@ class IndeedSpider(scrapy.Spider):
                 'location' : location
             }
 
+
+class sideDataSpider(scrapy.Spider):
+    name = 'IndeedSideData'
+    allowed_domains = ['indeed.fr']
+    start_urls = ['https://www.indeed.fr/jobs?q=data+scientist&start=','https://www.indeed.fr/jobs?q=data+analyst&start=','https://www.indeed.fr/jobs?q=data+engineer&start=']
+
+    def parse(self,response):
         for salaries in response.css("#SALARY_rbo").css('a'):
             salary = salaries.css("::attr(title)").extract()
             yield {
