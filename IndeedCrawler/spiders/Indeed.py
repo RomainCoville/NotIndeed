3c780c02da6f81f5b37140bb051deb8902fc2a75
coviller <romain.coville@edu.esiee.fr>
2019-01-29 19:09:18 +0100

    Premier crawler

--- IndeedCrawler/spiders/Indeed.py
+++ IndeedCrawler/spiders/Indeed.py
@@ -5,7 +5,19 @@ import scrapy
 class IndeedSpider(scrapy.Spider):
     name = 'Indeed'
     allowed_domains = ['indeed.fr']
-    start_urls = ['http://indeed.fr/']
+    start_urls = ['https://www.indeed.fr/jobs?q=data+scientist']
 
     def parse(self, response):
-        pass
+        for job in response.css(".jobsearch-SerpJobCard"):
+            title = (job.css(".jobtitle").css("::text").extract())
+            company = (job.css(".company").css("::text").extract())
+            location = (job.css(".location").css("::text").extract())
+            yield {
+                'title' : title,
+                'company' : company,
+                'location' : location
+            }
+
+    # def clean_spaces(self, string):
+    #     if string:
+    #         return string.split()
