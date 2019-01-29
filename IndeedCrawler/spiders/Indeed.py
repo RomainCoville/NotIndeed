720d7a4bb0643cc9178acc4d98d39163b7811afa
Steven POULAIN <poulains@pc5301-test.esiee.fr>
2019-01-29 19:10:37 +0100

    Salaires

--- IndeedCrawler/spiders/Indeed.py
+++ IndeedCrawler/spiders/Indeed.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+#import re
 
 class IndeedSpider(scrapy.Spider):
     name = 'Indeed'
     allowed_domains = ['indeed.fr']
-    start_urls = ['http://indeed.fr/']
+    start_urls = ['https://www.indeed.fr/Emplois-data-scientist']
 
     def parse(self, response):
+        # for item in response.css("#SALARY_rbo").css('a'):
+        #     print(re.findall("\d+", item.css("::attr(title)").extract_first()))
+        #     print(item.css("::attr(title)").extract())
+
+        response.css("#SALARY_rbo").css('a').css("::attr(title)").extract()
+
         pass
