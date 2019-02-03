2db8ec75612abe31d2a4092a589a3596805d34f6
coviller <romain.coville@edu.esiee.fr>
2019-02-03 21:13:09 +0100

    MAJ du docker lié à l'app

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM python:3.4-alpine
-ADD . /code
+FROM python:3.6
+
+RUN mkdir /code
 WORKDIR /code
-RUN pip install -r requirements.txt
-CMD ["python", "miaou.py"]
+
+COPY . .
+
+RUN pip install --upgrade pip && pip install -r requirements.txt
--- miaou.py
+++ miaou.py
@@ -40,25 +40,4 @@ def date():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-#! /usr/bin/python
-# -*- coding:utf-8 -*-
-
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
-
-
-@app.route('/')
-def accueil():
-    mots = ["Grosse Bite", "a", "toi,", "visiteur."]
-    return render_template('accueil.html', titre="Bienvenue !", mots=mots)
-
-@app.route('/date')
-def date():
-    d =  "a"
-    return render_template('date.html', la_date=d)
-
-
-if __name__ == '__main__':
     app.run("0.0.0.0",debug=True)
